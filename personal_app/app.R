f78d38e4d05cfb10e1d9e76ac07c38106cdcf12b
estherkho <estherkho2003@gmail.com>
2024-03-23 08:46:22 +0800

    23 Mar

--- personal_app/app.R
+++ personal_app/app.R
@@ -1,52 +1,44 @@
 library(shiny)
 library(ggplot2)
+library(shinythemes)
+library(readr)
 
+songs <- read_csv("spotify_songs_edited.csv")
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-  
-  # Application title
+  theme = shinytheme("cyborg"),
   titlePanel("Spotify Music"),
-  
-  # Sidebar layout with input and output definitions ----
   sidebarLayout(
-    
-    # Sidebar panel for inputs ----
     sidebarPanel(
-      
-      # Input: Selector for variable to plot against mpg ---
-    selectInput(inputId = "plot_variable_x",
-                label = "Select x-axis variable:",
-                choices = c("danceability","valence","speechiness","tempo")),
-    selectInput(inputId = "plot_variable_y",
-                label = "Select y-axis variable:",
-                choices = c("track_popularity"))
-  ),
-    
-    # Main panel for displaying outputs ----
+      titlePanel("Popularity vs Elements of Music"),
+      selectInput(inputId = "dataset", label = "Dataset", choices = c("songs")),
+      selectInput(inputId = "plot_variable_x",
+                  label = "Select x-axis variable:",
+                  choices = NULL),
+      selectInput(inputId = "plot_variable_y",
+                  label = "Select y-axis variable:",
+                  choices = NULL)
+    ),
     mainPanel(
-      # Output: Formatted text for caption ----
-      h3(textOutput("caption")),
-      
-      # Output: Plot of the requested variable against mpg ----
-      plotOutput("Popularity_Plot"),
-      )
+      h3(textOutput("caption", container = span)),
+      plotOutput("plot", width = "1000px", height = "800px") # Output: Plot based on selected variables
     )
+  )
 )
 
-# Define server logic required to draw a histogram
+# Define server logic to summarize and view selected dataset ----
 server <- function(input, output) {
   
-  # Compute the formula text ----
-  # This is in a reactive expression since it is shared by the
-  # output$caption and output$Popularity_Plot functions
-  formulaText <- reactive({
-    paste("popularity ~", input$plot_variable_x)
+  # Return the requested dataset ----
+  datasetInput <- reactive({
+    switch(input$dataset,
+           "songs" = songs)
   })
   
-  # Return the formula text for printing as a caption ----
+  # Create caption ----
   output$caption <- renderText({
-    formulaText()
+    input$caption
   })
   
   # Generate a summary of the dataset (conditional) ----
@@ -65,15 +57,19 @@ server <- function(input, output) {
       choices = names(dataset),
       selected = NULL
     )
-    
-    if (input$dataset == "music") {
+    if (input$dataset == "songs") {
       updateSelectInput(
         inputId = "plot_variable_x",
         choices = c("danceability","valence","speechiness","tempo"),
         selected = NULL
       )}
-
-})
+    updateSelectInput(
+      inputId = "plot_variable_y",
+      choices = c("track_popularity"),
+      selected = NULL
+    )
+  }
+  )
   
   # Generate plot based on selected variables ----
   
@@ -86,11 +82,12 @@ server <- function(input, output) {
       ggplot(dataset, aes_string(x = variable_x, y = variable_y)) +
         geom_point() +
         labs(x = variable_x, y = variable_y) +
+        theme_minimal() +
         geom_smooth(method = "lm")
     }
   })
+  
 }
-
 
 # Run the application 
 shinyApp(ui = ui, server = server)
